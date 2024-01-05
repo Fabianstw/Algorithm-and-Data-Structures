3bd3fab0dcc351efcb90e147884c704a4166ec61
Fabian1032908ru <fstiewe@aol.com>
2024-01-05 08:18:22 +0100

    New LC P

---
+++ leetcode/cpp/Week1_Problems/FindTheStudentThatWillReplaceTheChalk_1894.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Fabian Stiewe on 05.01.24.
+//
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+/**
+ * Beats around 80%
+ */
+
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+
+        long chalk_sum = 0;
+        for (int i = 0; i < chalk.size(); i++) {
+            if (chalk_sum > k) {
+                return i-1;
+            }
+            chalk_sum += chalk[i];
+        }
+        k %= chalk_sum;
+
+        for (int i = 0; i < chalk.size(); i++) {
+            k -= chalk[i];
+            if (k < 0) {
+                return i;
+            }
+        }
+
+        return 0;
+
+    }
+};
--- leetcode/cpp/main.cpp
+++ leetcode/cpp/main.cpp
@@ -7,9 +7,15 @@
 #include "Week1_Problems/SearchInRotatedSortedArray_33.cpp"
 #include "Week1_Problems/MaximumAreaofaPieceofCakeAfterHorizontalandVerticalCuts_1465.cpp"
 #include "Week1_Problems/UniquePathes_62.cpp"
+#include "Week1_Problems/FindTheStudentThatWillReplaceTheChalk_1894.cpp"
 
 int main()
 {
+
+    vector<int> nums = {5,1,5};
+    Solution sol;
+
+    cout << sol.chalkReplacer(nums, 22);
 
     return 0;
 
